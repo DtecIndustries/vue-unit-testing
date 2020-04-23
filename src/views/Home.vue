6f4f96d39ce194263cb143465e1afc0424eb0896
Joachim Defour <joachim.defour@icloud.com>
2020-04-23 20:36:03 +0200

    RandomNumber unit test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <AppHeader />
     <img alt="Vue logo" src="../assets/logo.png" />
+    <RandomNumber />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import AppHeader from "@/components/AppHeader.vue";
+import RandomNumber from "@/components/RandomNumber.vue";
 
 export default {
   name: "Home",
   components: {
-    AppHeader
+    AppHeader,
+    RandomNumber
   }
 };
 </script>
